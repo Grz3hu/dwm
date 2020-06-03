f47eb1b5f8964b2166a4c153648971e89a2b9aaf
Grz3hu <grek222@o2.pl>
2020-06-03 17:55:33 +0200

    attach bottom patch

--- config.h
+++ config.h
@@ -8,15 +8,6 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=14" };
 static const char dmenufont[]       = "monospace:size=12";
-/* static const char col_gray1[]       = "#222222"; */
-/* static const char col_gray2[]       = "#444444"; */
-/* static const char col_gray3[]       = "#bbbbbb"; */
-/* static const char col_gray4[]       = "#eeeeee"; */
-/* static const char col_cyan[]        = "#005577"; */
-/* static const char *colors[][3]      = { */
-/* 	/1*               fg         bg         border   *1/ */
-/* 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 }, */
-/* 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  }, */
 static char normbgcolor[]           = "#2e3440";
 static char normbordercolor[]       = "#2e3440";
 static char normfgcolor[]           = "#bbbbbb";
@@ -50,7 +41,7 @@ static const Rule rules[] = {
 	{ "feh",     		NULL,       NULL,       0,            1,		1,           -1 },
 	{ "st",     		NULL,       NULL,       0,            1,		0,           -1 },
 	{ "Steam",     		NULL,       NULL,       4,            0,		1,           -1 },
-	{ "csgo_linux64", 	NULL,       NULL,       3,            0,		1,           -1 },
+	{ "csgo_linux64", 	NULL,       NULL,       0,            0,		1,           -1 },
 	{ "float",     		NULL,       NULL,       0,            1,		1,           -1 },
 	{ "Firefox",  		NULL,       NULL,       2,            0,		0,           -1 },
 };
@@ -83,7 +74,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-p", "Run: ", NULL }; */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor,"-c", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor,NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *termfloat[]  = { "st","-c","float", NULL };
 static const char *firefox[]  = { "firefox-bin", NULL };
